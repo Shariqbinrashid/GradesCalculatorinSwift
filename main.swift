0cd27a2af95bc50f5b45e21097c8ce27ab0f180e
Shariqbinrashid <shariqbinrashid@gmail.com>
2020-11-07 06:26:08 +0500

    Final

--- main.swift
+++ main.swift
@@ -29,8 +29,12 @@ let contents = try! String(contentsOfFile: filename)
 // Split the file into separate lines
 let lines = contents.split(separator:"\n") //splitting rows seperated by \n
 for line in lines {
-   // print("\(line) ")
-    let rows = line.split(separator:",")//splitting row seperated by ,
+    print("\(line) ")
+    if line.range(of:"C#000000") != nil{
+
+    }
+    else{
+   let rows = line.split(separator:",")//splitting row seperated by ,
    data.removeAll()
     for  row in rows{
      // print("\(row) ")
@@ -50,22 +54,76 @@ finalExamMarksx=Int(data[5])!
 s.append(student(studentId:studentIdx,claMarks:claMarksx,olaMarks:olaMarksx,quizMarks:quizMarksx,examMarks:examMarksx,finalExamMarks:finalExamMarksx))
 r.objects.append(s[j]) //storing runtime objects in objects array of roasters
 j+=1
+    }
+ 
 }
 
 
 
 }
 
+func OutputFromFile() {  
+  //local variables
 
+var s:student 
+var studentIdx: String
+var claMarksx: Int=0 
+var olaMarksx: Int=0 
+var quizMarksx: Int=0 
+var examMarksx: Int=0
+var finalExamMarksx: Int=0
+var data = [String]()
+//reading from file
+// Determine the file name
+let filename = "student.dat"
+
+// Read the contents of the specified file
+
+let contents = try! String(contentsOfFile: filename)
+
+// Split the file into separate lines
+let lines = contents.split(separator:"\n") //splitting rows seperated by \n
+for line in lines {
+    print("\(line) ", terminator: " ")
+      if line.range(of:"C#000000") != nil{
+      print("")
+    }
+   else{
+     
+ let rows = line.split(separator:",")//splitting row seperated by ,
+   data.removeAll()
+    for  row in rows{
+     // print("\(row) ")
+      
+      data.append(String(row))
+      
+    }
+
+studentIdx=String(data[0])
+claMarksx=Int(data[1])!
+olaMarksx=Int(data[2])!
+quizMarksx=Int(data[3])!
+examMarksx=Int(data[4])!
+finalExamMarksx=Int(data[5])!
+
+s=student(studentId:studentIdx,claMarks:claMarksx,olaMarks:olaMarksx,quizMarks:quizMarksx,examMarks:examMarksx,finalExamMarks:finalExamMarksx)
+print("GRADE: ",r.grade(s:s))
+
+   }
+}
+
+
+
+}
+print("Input File students.dat")
 readinfFromFile()
-print(r.claAverage())
-print(r.olaAverage())
-print(r.quizAverage())
-print(r.examAverage())
-print(r.finalExamAverage())
-print(r.claHighest())
-print(r.olaHighest())
-print(r.quizHighest())
-print(r.examHighest())
-print(r.result())
+
+print("\n Output File")
+OutputFromFile()
+
+print("Average ",String(format: "%.1f", r.claAverage()),String(format: "%.1f", r.olaAverage()),String(format: "%.1f", r.quizAverage()),String(format: "%.1f", r.examAverage()),String(format: "%.1f", r.finalExamAverage()))
+
+print("Highest ",r.claHighest(),r.olaHighest(),r.quizHighest(),r.examHighest(),r.examHighest())
+
+
 //print(String(format: "%.1f", r.olaAverage()))
---
+++ readme.txt
@@ -0,0 +1,2 @@
+This application takes students.data file as input and calulate Average , Highest and Grades of students.
+ Input and output snapshot is attached.
